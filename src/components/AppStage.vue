f7a647b6c2196b28ee391e558a4039c838b79f14
drinkingtheink <jason.m.harrison@gmail.com>
2023-09-15 16:52:52 -0500

    switched to hsl for color selection

--- src/components/AppStage.vue
+++ src/components/AppStage.vue
@@ -1,84 +1,82 @@
 <template>
   <main>
-    <section>
-      <h2>Color Selection</h2>
-      <input type="color" @input="updateactiveHex" />
-    </section>
-    <section>
-      <div v-show="activeHex" class="active-color-display hex" :style="activeHexDisplay" />
+    <h2>Make a Color in HSL</h2>
 
-      <div class="rgb">
-        <p>{{ activeRGB }}</p>
-      </div>
+    <div class="color-display" :style="colorDisplay"/>
 
-      <div class="hsl">
-        <p>{{ activeHSL }}</p>
+    <div class="wrapper hue">
+      <input 
+        type="range" 
+        id="hue-input" 
+        name="hue-input" 
+        min="0" 
+        max="359" 
+        @input="updateHue"
+      />
+      <label for="hue-input">Hue</label>
+    </div>
 
-        <div class="active-color-display hsl" :style="activeHSLDisplay" />
-      </div>
-    </section>
+    <div class="wrapper sat">
+      <input 
+        type="range" 
+        id="sat-input" 
+        name="sat-input" 
+        min="0" 
+        max="100" 
+        value="100" 
+        @input="updateSat"
+      />
+      <label for="sat-input">Saturation</label>
+    </div>
+
+    <div class="wrapper light">
+      <input 
+        type="range" 
+        id="light-input" 
+        name="light-input" 
+        min="0" 
+        max="100" 
+        value="50" 
+        @input="updateLight"
+      />
+      <label for="light-input">Lightness</label>
+    </div>
   </main>
 </template>
 
 <script>
+// import chroma from "chroma-js";
+
 export default {
   name: 'AppStage',
   data() {
     return {
-      activeHex: null,
-      activeHSL: null,
+      hue: 0,
+      saturation: 100,
+      lightness: 50,
     }
   },
   methods: {
-    updateactiveHex(e) {
-      this.activeHex = e.target.value;
-      this.deriveHSL();
+    updateHue(e) {
+      this.hue = e.target.value;
     },
-    hexToRgb(hex) {
-      return hex ? hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-                ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-        .substring(1).match(/.{2}/g)
-        .map(x => parseInt(x, 16)) : null
+    updateSat(e) {
+      this.saturation = e.target.value;
     },
-    deriveHSL() {
-      this.activeRGB ? this.activeHSL = this.rgbToHSL(this.activeRGB) : null;
-    },
-    rgbToHSL ([r, g, b]) {
-      r /= 255;
-      g /= 255;
-      b /= 255;
-      const l = Math.max(r, g, b);
-      const s = l - Math.min(r, g, b);
-      const h = s
-        ? l === r
-          ? (g - b) / s
-          : l === g
-          ? 2 + (b - r) / s
-          : 4 + (r - g) / s
-        : 0;
-      return [
-        Math.round(60 * h < 0 ? 60 * h + 360 : 60 * h),
-        Math.round(100 * (s ? (l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s))) : 0)) + '%',
-        Math.round((100 * (2 * l - s)) / 2) + '%',
-      ];
+    updateLight(e) {
+      this.lightness = e.target.value;
     },
   },
   computed: {
-    activeHexDisplay() {
-      return `background: ${this.activeHex}`;
-    },
-    activeRGB() {
-      return this.hexToRgb(this.activeHex);
-    },
-    activeHSLDisplay() {
-      return `background-color: ${this.activeHSL}`;
+    colorDisplay() {
+      return `background-color: hsl(${this.hue}deg ${this.saturation}% ${this.lightness}%)`;
     },
   },
 }
 </script>
 
 <style>
-.active-color-display {
+.color-display {
   padding: 4rem;
   margin: 2rem auto 0 auto;
   width: 20rem;
