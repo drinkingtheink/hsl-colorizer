f8dcd1bc393995ae23a026810bbd03b7da9cb9f0
drinkingtheink <jason.m.harrison@gmail.com>
2023-09-16 20:29:47 -0500

    about links

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <AppStage/>
+  <AppStage :lightOrDark="lightOrDark"/>
+  <footer>
+    <a href="https://github.com/drinkingtheink/hsl-colorizer">About this App</a>
+    <a href="https://www.drinkingtheink.com/?topic=about">About the Author</a>
+  </footer>
 </template>
 
 <script>
@@ -10,6 +14,47 @@ export default {
   components: {
     AppStage
   },
+  methods: {
+    lightOrDark(color) {
+      // Variables for red, green, blue values
+      var r, g, b, hsp;
+      
+      // Check the format of the color, HEX or RGB?
+      if (color.match(/^rgb/)) {
+
+          // If RGB --> store the red, green, blue values in separate variables
+          color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+          
+          r = color[1];
+          g = color[2];
+          b = color[3];
+      } 
+      else {
+          // If hex --> Convert it to RGB: http://gist.github.com/983661
+          color = +("0x" + color.slice(1).replace( 
+          color.length < 5 && /./g, '$&$&'));
+
+          r = color >> 16;
+          g = color >> 8 & 255;
+          b = color & 255;
+      }
+      
+      // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+      hsp = Math.sqrt(
+      0.299 * (r * r) +
+      0.587 * (g * g) +
+      0.114 * (b * b)
+      );
+
+      // Using the HSP value, determine whether the color is light or dark
+      if (hsp>127.5) {
+          return 'light';
+      } 
+      else {
+          return 'dark';
+      }
+  },
+  }
 }
 </script>
 
@@ -52,5 +97,26 @@ button {
 button:hover {
   color: white;
   box-shadow: 0 0 20px var(--primary);
+}
+
+footer {
+  padding-top: 4rem;
+}
+
+footer a {
+  display: inline-block;
+  color: var(--primary);
+  padding: 1rem 2rem;
+  background-color: var(--transBg);
+  margin: 0 5px 5px 0;
+  color: white;
+  text-decoration: none;
+  transition: all 0.5s;
+  border-left: 5px solid var(--primary)
+}
+
+footer a:hover {
+  background-color: var(--primary);
+  border-color: violet;
 }
 </style>
